a5242bfd237710c460344811f710e029b6d68c0b
amilsaleem <31254366+amilsaleem@users.noreply.github.com>
2020-07-18 02:28:20 +0530

    First Assignment

---
+++ app.js
@@ -0,0 +1,26 @@
+(function() {
+  "use strict";
+
+  angular.module('Meal',[])
+  .controller('mealMenu',eat);
+  eat.$inject=['$scope'];
+  function eat($scope) {
+    $scope.check = function () {
+      var totalmeal=$scope.name;
+      if (totalmeal==undefined) {
+        $scope.msg="Please Enter Data First";
+      }
+      else {
+      var meal=totalmeal.split(',');
+      if (meal.length<=3) {
+        $scope.msg="Enjoy!";
+      }
+      else if (meal.length>3) {
+        $scope.msg="Too Much!";
+      }
+      }
+      console.log();
+    }
+  }
+
+})();
